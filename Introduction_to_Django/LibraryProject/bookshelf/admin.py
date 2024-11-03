3b85903d9042d760952ec80f24ffd82b381519e7
Jubilantrich <jubilantrich@gmail.com>
2024-11-03 08:15:54 +0000

    Add my task 3

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -7,6 +7,6 @@ from .models import Product
 class BookAdmin(admin.ModelAdmin):
     list_display =('title', 'author','published_date')
     search_fields=('title','author')
-    list_fliter=('publication_year')
-    
+    list_filter=('publication_year')
+
 admin.site.register(Book, BookAdmin)
