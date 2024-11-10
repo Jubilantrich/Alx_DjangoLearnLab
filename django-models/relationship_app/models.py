b1a953b431b9f8028ed6ded377b4a08e0864180d
Jubilantrich <jubilantrich@gmail.com>
2024-11-10 05:59:10 +0000

    just for the checker

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -6,20 +6,29 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 #Book Model
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
+    def __str__(self):
+        return self.name
+    
 #Optimising queries with prefetching
 books = Book.objects.prefetch_related('Author')
 
-
+    
 #Library Model
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, on_delete=models.CASCADE, related_name='libraries')
 
+    def __str__(self):
+        return self.name
+    
 #Optimising queries with prefetching
 libraries = Library.objects.prefetch_related('Book')
 
@@ -27,6 +36,9 @@ libraries = Library.objects.prefetch_related('Book')
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarians')
+
+    def __str__(self):
+        return self.name
 
 #Optimising queries with prefetching
 librarians = Librarian.objects.prefetch_related('Library')
