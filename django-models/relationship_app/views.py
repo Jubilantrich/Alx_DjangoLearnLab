3a10752fdecec74b3d2420c3883ab75e67878b7e
RICHARD DOK <jubilantrich@gmail.com>
2024-11-10 18:42:08 +0000

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import BookForm  # Assuming you have a BookForm for handling form data
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
 
 # Create your views here.
 
@@ -74,4 +76,25 @@ def delete_book(request, pk):
         return redirect('book_list')
     return render(request, 'relationship_app/delete_book.html', {'book': book})
 
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
 
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+# Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
