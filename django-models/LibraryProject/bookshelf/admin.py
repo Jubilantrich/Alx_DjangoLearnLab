118515215aaab13add88e3ebe1c35fb8871770d4
Jubilantrich <jubilantrich@gmail.com>
2024-11-10 01:12:35 +0000

    Week 10 task 0

---
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Book
+from .models import Product
+
+# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display =('title', 'author','published_date')
+    search_fields=('title','author')
+    list_filter=('publication_year')
+
+admin.site.register(Book, BookAdmin)
