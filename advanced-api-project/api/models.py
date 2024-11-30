e2baa6e75fa7be605639c867eee0291182f527b4
Jubilantrich <jubilantrich@gmail.com>
2024-11-30 07:50:00 +0000

    update on week 13

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -27,7 +27,7 @@ class Book(models.Model):
     """
     title = models.CharField(max_length=200)
     publication_year =models.IntegerField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    author = models.ForeignKey(Author, many=True, read_only=True, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self) -> str:
         return f"'{self.title}' by {self.author}"
