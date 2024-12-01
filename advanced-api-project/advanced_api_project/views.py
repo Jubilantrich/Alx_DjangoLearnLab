58d5a1505dc69fb20f0dcbb3f12a2a262a7f1ea2
Jubilantrich <jubilantrich@gmail.com>
2024-12-01 20:55:16 +0000

    update on week 13

---
+++ advanced-api-project/advanced_api_project/views.py
@@ -0,0 +1,32 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from api.models import Book
+from api.serializers import BookSerializer
+
+#List View
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+#DatailView
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookListView
+
+#CreateView
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+#updateView
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+#DeleteView
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
