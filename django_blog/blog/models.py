d95ac59729f5a4703e4c225adc1ea3a7b99d85a5
Jubilantrich <jubilantrich@gmail.com>
2024-12-04 20:00:33 +0000

    WEEK 14 UPDATE

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    email = models.EmailField()
-    passwrd = models.CharField(max_length=255)
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
