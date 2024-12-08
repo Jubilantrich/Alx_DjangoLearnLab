877a8ac521406090f4226bbc624b538901e30602
Jubilantrich <jubilantrich@gmail.com>
2024-12-08 14:55:45 +0000

    WEEK 14 UPDATE

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,9 +14,9 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('accounts/profile/', userProfile, name='profile'),
     path('', Post_ListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', Post_DetailView.as_view(), name='post-detail'),
-    path('posts/new/', Post_CreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/edit/', Post_UpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', Post_DeteteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/', Post_DetailView.as_view(), name='post-detail'),
+    path('post/new/', Post_CreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', Post_UpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', Post_DeteteView.as_view(), name='post-delete'),
 
 ]
