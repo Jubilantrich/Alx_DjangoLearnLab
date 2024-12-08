faa0991258677871ea160e2d54c91e84b12544af
Jubilantrich <jubilantrich@gmail.com>
2024-12-08 22:35:21 +0000

    WEEK 14 UPDATE

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
     path('search/', search_posts, name='search-posts'),
-    path('tags/<str:tag_name>/', posts_by_tag, name='posts-by-tag'),
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts-by-tag'),
 
 
 
