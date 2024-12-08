ba830a3b9aa5e19b0273c6ffc4e205600a6943a9
RICHARD DOK <jubilantrich@gmail.com>
2024-12-08 21:03:06 +0000

    Update urls.py
    week 14 update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -22,9 +22,9 @@ urlpatterns = [
     path('post/new/', Post_CreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', Post_UpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', Post_DeteteView.as_view(), name='post-delete'),
-    path('posts/<int:post_id>/comments/new/', add_comment, name='add-comment'),
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:post_id>/comments/new/', add_comment, name='add-comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 
 
 ]
