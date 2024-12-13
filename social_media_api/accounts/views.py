b61745a5017e759382b7202cb335a9815221365c
Jubilantrich <jubilantrich@gmail.com>
2024-12-13 07:02:18 +0000

    Week 15 capstone

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, viewsets, permissions
+from rest_framework import status
 from rest_framework.authtoken.models import Token
-from .serializers import UserSerializer, RegisterSerializer, PostSerializer, CommentSerializer
-from .models import CustomUser, Post, Comment
+from .serializers import UserSerializer, RegisterSerializer
+from .models import CustomUser
 
 
 # API View for user registration
--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,  # Reference the custom user model
         on_delete=models.CASCADE,  # Delete posts when the author is deleted
-        related_name='posts'  # Allows reverse lookup (user.posts)
+        related_name='account_posts'  # Allows reverse lookup (user.posts)
     )
     title = models.CharField(max_length=200)  # Title of the post
     content = models.TextField()  # Content of the post
@@ -20,15 +20,15 @@ class Post(models.Model):
 class Comment(models.Model):
     # ForeignKey relationship linking Comment to a Post
     post = models.ForeignKey(
-        Post, 
+        'Post',  # Reference to the Post model
         on_delete=models.CASCADE,  # Delete comments when the post is deleted
-        related_name='comments'  # Allows reverse lookup (post.comments)
+        related_name='post_comments'  # Updated related_name to avoid conflict
     )
     # ForeignKey relationship linking Comment to the User who created it
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL, 
+        settings.AUTH_USER_MODEL,  # Reference to the custom user model
         on_delete=models.CASCADE,  # Delete comments when the author is deleted
-        related_name='comments'  # Allows reverse lookup (user.comments)
+        related_name='user_comments'  # Updated related_name to avoid conflict
     )
     content = models.TextField()  # Content of the comment
     created_at = models.DateTimeField(auto_now_add=True)  # Timestamp when comment is created
@@ -37,3 +37,4 @@ class Comment(models.Model):
     def __str__(self):
         # String representation of the Comment
         return f"Comment by {self.author.username} on {self.post.title}"
+    
